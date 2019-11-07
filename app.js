63a2f144be6e678b1e1818f3783fa871d01cd0f8
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2019-11-06 16:36:38 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var user = prompt('What is your name?')
+console.log('the users name is ' + user );
+
+alert('Hi there '+ user + ' ! Welcome to my page');
+
+// first gusses question
+var hair = prompt('Is my hair color black').toLowerCase();
+console.log('color of hair', hair);
+
+if (hair === 'yes'){
+    alert('You got it!')
+} 
+
+if (hair === 'no'){
+    alert('Nope, my hair color is black!')
+}
+
+
+// second guess question
+var eyes = prompt('Is my eye color blue?');
+console.log('color of eyes', eyes);
+
+if (eyes === 'yes'){
+    alert('No, my eyes color is Brown')
+} 
+
+if (eyes === 'no'){
+    alert('You are correct, my eyes color is brown')
+}
+
+//third guess question
+var dog = prompt('Do I have a dog?');
+console.log('if i have a dog', dog);
+
+if (dog === 'yes'){
+    alert('Correct! I have a french bulldog ')
+} 
+
+if (dog === 'no'){
+    alert('I do have a french bulldog')
+}
+
+//fourth guess question
+var kid = prompt('Do I have a kid?');
+console.log('if i have a kid', kid);
+
+if (kid === 'yes'){
+    alert('Right!')
+}
+
+if (kid === 'no'){
+    alert('I have a 8 months baby boy')
+}
+
+//fifth guess question 
+
+var SDE = prompt('Am I a SDE?');
+console.log('if i am a SDE', SDE);
+
+if (SDE === 'yes') {
+    alert('You must know that from my Linkedln')
+}
+
+if (SDE === 'no'){
+    alert('I just became a SDE since I learned coding from code fellows') 
+}
+
