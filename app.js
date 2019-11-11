80b9b3d5cf291ffc447bacceaa566ed8d146c17a
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2019-11-11 15:09:02 -0800

    updated question seven

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 'use strict';
+/* 
 var correct = 0
 
 var user = prompt('What is your name?')
-console.log('the users name is ' + user );
+console.log('the users name is ' + user);
 
-alert('Hi there '+ user + ' ! Welcome to my page');
+alert('Hi there ' + user + ' ! Welcome to my page' + ' ! Lets play a game, only yes/no answers will be accepted');
 
 // first gusses question
 var hair = prompt('Is my hair color black').toLowerCase();
 console.log('color of hair', hair);
 
-if (hair === 'yes'){
+if (hair === 'yes') {
     alert('You got it!')
     correct++
-} 
+}
 
-if (hair === 'no'){
+if (hair === 'no') {
     alert('Nope, my hair color is black!')
 }
 
@@ -24,12 +25,12 @@ if (hair === 'no'){
 var eyes = prompt('Is my eye color blue?').toLowerCase();
 console.log('color of eyes', eyes);
 
-if (eyes === 'yes'){
+if (eyes === 'yes') {
     alert('No, my eyes color is Brown')
     correct++
-} 
+}
 
-if (eyes === 'no'){
+if (eyes === 'no') {
     alert('You are correct, my eyes color is brown')
 }
 
@@ -37,12 +38,12 @@ if (eyes === 'no'){
 var dog = prompt('Do I have a dog?').toLowerCase();
 console.log('if i have a dog', dog);
 
-if (dog === 'yes'){
+if (dog === 'yes') {
     alert('Correct! I have a french bulldog ')
     correct++
-} 
+}
 
-if (dog === 'no'){
+if (dog === 'no') {
     alert('I do have a french bulldog')
 }
 
@@ -50,12 +51,12 @@ if (dog === 'no'){
 var kid = prompt('Do I have a kid?').toLowerCase();
 console.log('if i have a kid', kid);
 
-if (kid === 'yes'){
+if (kid === 'yes') {
     alert('Right!')
     correct++
 }
 
-if (kid === 'no'){
+if (kid === 'no') {
     alert('I have a 8 months baby boy')
 }
 
@@ -69,61 +70,66 @@ if (SDE === 'yes') {
     correct++
 }
 
-if (SDE === 'no'){
-    alert('I just became a SDE since I learned coding from code fellows') 
+if (SDE === 'no') {
+    alert('I just became a SDE since I learned coding from code fellows')
 }
 
 //sixth guess question
 var number;
 var counter = 1;
 
-while(number !== 6) {
-  number = parseInt(prompt('Guess,what is my fav number beween 1-10?'));
+while (number !== 6) {
+    number = parseInt(prompt('Guess,what is my fav number beween 1-10?'));
 
-  if (number === NaN || number === null) {
-    alert('please enter a real number');
-    counter++;
-  }else if (number < 6) {
-    alert('you guessed too low');
-    counter++;
-  } else if (number > 6) {
-    alert('you guessed too high');
-    counter++;
-  }else if (number = 6){
-      alert('correct')
-      correct++
-      break;
-  }
-  if (counter === 5){
-      alert('My fav number is actually 6');
-      break;
-  }
-  }
-    
-console.log('current count:', counter);
-
-// seventh guess question
-var company = ['Google','Amazon','Facebook','Microsoft','Apple','Cisco System'];
-var answer = prompt('what is your fav tech company');
-var counter = 1
-
-for(var i = 0; i < company.length; i++){
-    console.log('fav tech company', company[i]);
-
-    if (answer === company[i]){
-        alert('Great! I like' + '' + company[i] + 'too');
-        counter++
+    if (number === NaN || number === null) {
+        alert('please enter a real number');
+        counter++;
+    } else if (number < 6) {
+        alert('you guessed too low');
+        counter++;
+    } else if (number > 6) {
+        alert('you guessed too high');
+        counter++;
+    } else if (number = 6) {
+        alert('correct')
         correct++
         break;
-    } else if (answer !== company[i]){
-        prompt('try again')
-        counter++
     }
-        if (counter >= 6 ){
-            alert('Those are what we have' + '' + company)
-            break;
-        }
+    if (counter === 5) {
+        alert('My fav number is actually 6');
+        break;
+    }
 }
 
+console.log('current count:', counter);
+*/
+// seventh guess question
 
-alert('Thank you for visiting my page '+ user + ' you are corrected for ' + correct +' out of 7 questions' + ' ! See you next time!');
+var company = ['Google', 'Amazon', 'Facebook', 'Microsoft', 'Apple', 'Cisco System'];
+var answer = prompt('what is your fav tech company');
+var counter = 1
+var user = 'Anna' // note fake name
+var correct= 0 // note fake #
+
+while (counter <= 6 ) {
+
+        if (company.includes(answer)) {
+            alert('Great! I like' + '' + answer + 'too');
+            correct++
+            break;
+        }else {
+            answer=prompt ('try again') 
+            counter++
+            
+        }
+
+       if (counter === 6){
+        alert('Those are what we have' + '' + company)
+        break;
+       }
+
+    }
+    
+
+
+alert('Thank you for visiting my page ' + user + ' you are corrected for ' + correct + ' out of 7 questions' + ' ! See you next time!');
